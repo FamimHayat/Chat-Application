0a34ab6515e1ebf63fe23c94f0ac94c9a3505c4f
famim2002 <famimhayatyash@gmail.com>
2025-06-17 21:15:37 +0600

    user page styling

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const UserProfile = () => {
   return (
-      <>
-          <section className='bg-primary h-dvh w-full'>
-              
-          </section>
-      </>
-  )
-}
+    <>
+      <section className="flex items-center justify-center">
+        <a
+          href="#"
+          className="group relative flex h-[321px] w-[220px] flex-col items-center justify-center overflow-hidden rounded-tr-[10px] bg-white text-[#4C5656] shadow-[0_14px_26px_rgba(0,0,0,0.04)] transition-all duration-300 ease-out hover:translate-y-[-5px] hover:scale-[1.005] hover:shadow-[0_24px_36px_rgba(0,0,0,0.11),0_24px_46px_rgba(220,233,255,0.48)] active:scale-[1] active:shadow-[0_15px_24px_rgba(0,0,0,0.11),0_15px_24px_rgba(220,233,255,0.48)]"
+        >
+          <div className="absolute top-[70px] left-[50px] h-[118px] w-[118px] rounded-full bg-[#DCE9FF] transition-transform duration-300 ease-out group-hover:scale-[4]" />
 
-export default UserProfile
+          <div className="relative z-10 flex h-[131px] w-[131px] items-center justify-center rounded-full border-4 border-[#DCE9FF] bg-white transition-all duration-300 ease-out group-hover:border-[#f1f7ff] group-hover:bg-[#DCE9FF]">
+            <div className="absolute h-[118px] w-[118px] rounded-full bg-[#DCE9FF] transition-opacity duration-300 ease-out group-hover:bg-[#f1f7ff]" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="1855 26 66 77"
+              height="77px"
+              width="66px"
+              className="z-10"
+            >
+              <desc>Created with Sketch.</desc>
+              <g
+                transform="translate(1855.000000, 26.000000)"
+                fillRule="evenodd"
+                fill="none"
+                strokeWidth={1}
+                stroke="none"
+                id="Page-1"
+              >
+                <path
+                  fill="#AFCEFF"
+                  id="Fill-8"
+                  d="M4.28872448,42.7464904 C4.28872448,39.3309774 5.4159227,33.7621426 6.40576697,30.4912557 C10.5920767,32.1098991 14.3021264,35.1207513 18.69596,35.1207513 C30.993618,35.1207513 42.5761396,28.7162991 49.9992251,17.9014817 C56.8027248,23.8881252 60.8188351,33.0463165 60.8188351,42.7464904 C60.8188351,60.817447 47.6104607,76.6693426 32.5537798,76.6693426 C17.4970989,76.6693426 4.28872448,60.817447 4.28872448,42.7464904"
+                />
+                <path
+                  fill="#3B6CB7"
+                  id="Fill-10"
+                  d="M64.3368879,31.1832696 L62.8424171,46.6027478 L60.6432609,46.7824348 L59.8340669,34.6791304 L47.6573402,25.3339478 C44.2906753,34.068487 34.3459503,40.2903304 24.4684093,40.2903304 C17.7559812,40.2903304 10.046244,37.4168 5.80469412,32.8004522 L5.80469412,34.6791304 L5.80469412,46.6027478 L4.28932167,46.6027478 L1.30187314,27.8802435 C1.30187314,20.9790957 3.52342407,15.5432 7.27229127,11.3578087 C13.132229,4.79558261 21.8124018,0.0492173913 30.5672235,0.342852174 C37.4603019,0.569286957 42.6678084,2.72991304 50.8299179,0.342852174 C51.4629405,1.44434783 51.8615656,3.00455652 51.5868577,5.22507826 C51.4629405,6.88316522 51.2106273,7.52302609 50.8299179,8.45067826 C58.685967,14.1977391 64.3368879,20.7073739 64.3368879,31.1832696"
+                />
+                <path
+                  fill="#568ADC"
+                  id="Fill-13"
+                  d="M58.9405197,54.5582052 C62.0742801,54.8270052 65.3603242,52.60064 65.6350321,49.5386574 C65.772386,48.009127 65.2617876,46.5570226 64.3182257,45.4584487 C63.3761567,44.3613357 62.0205329,43.6162922 60.4529062,43.4818922 L58.9405197,54.5582052 Z"
+                />
+                <path
+                  fill="#568ADC"
+                  id="Fill-15"
+                  d="M6.32350389,54.675367 C3.18227865,54.8492104 0.484467804,52.4957496 0.306803449,49.4264626 C0.217224782,47.8925496 0.775598471,46.4579757 1.75200594,45.3886191 C2.7284134,44.3192626 4.10792487,43.6165843 5.67853749,43.530393 L6.32350389,54.675367 Z"
+                />
+              </g>
+            </svg>
+          </div>
+          <p className="mt-[30px] text-[17px] text-inherit transition-colors duration-300 ease-out group-hover:text-[#4C5656] z-10">
+            Human Resources
+          </p>
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default UserProfile;
