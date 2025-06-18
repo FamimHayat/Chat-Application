a9a9371cc844f8b61f921d8557c930b34a19846f
famim2002 <famimhayatyash@gmail.com>
2025-06-18 13:11:34 +0600

    react-redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { dbConfig } from '../fbConfig.js'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
-  
+import store from './redux-store/store.js'
+
+
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  
-)
+  </Provider>
+);
