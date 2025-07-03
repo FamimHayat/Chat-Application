367890b7973bc00e4fc74431f866af2c32577917
famim2002 <famimhayatyash@gmail.com>
2025-07-03 09:45:01 +0600

    styling

--- src/componenets/ChatList.jsx
+++ src/componenets/ChatList.jsx
@@ -6,16 +6,24 @@ const ChatList = () => {
   return (
     <>
       <section className="h-dvh md:w-4xl flex flex-col  bg-primary  border-r-2 border-r-brand rounded-xl">
-        <div className="flex border-b-2 border-b-brand rounded-2xl p-3 gap-3">
-          <input
-            type="text"
-            placeholder="user name "
-            className="placeholder:text-white text-[22px] w-full  p-3 text-text rounded-full bg-primaryColor outline-0 input-shadow border-3 border-secondary focus:outline-3 focus:outline-brand "
-          />
-          <div className=" chat-search flex justify-center items-center p-5 mx-2 hover:bg-base border-2 border-base bg-primary rounded-full group cursor-pointer">
-            <FaSearch className="text-[20px] text-base group-hover:text-primary" />
+        <div className="flex items-center w-full border-b-2 border-b-brand rounded-2xl">
+          <div className="relative flex  w-full   p-3 gap-3">
+            <input
+              type="text"
+              placeholder="user name"
+              className="placeholder:text-white text-[22px] w-full p-3 text-text rounded-full bg-primaryColor outline-0 input-shadow border-3 border-secondary focus:outline-3 focus:outline-brand"
+            />
+            <div className="absolute right-6 top-1/2 transform -translate-y-1/2 flex justify-center items-center p-2 hover:bg-base border-2 border-base bg-primary rounded-full group cursor-pointer">
+              <FaSearch className="text-[20px] text-base group-hover:text-primary" />
+            </div>
+          </div>
+          <div className="p-2">
+            <button className="text-2xl font-semibold py-2 px-4 border-2  mr-3 cursor-pointer  text-brand hover:text-primary hover:bg-text">
+              add
+            </button>
           </div>
         </div>
+
         <div className="grid grid-cols-1 gap-4  py-2 my-3 overflow-y-scroll">
           <Users
             userImage={"/user-image (1).jpg"}
